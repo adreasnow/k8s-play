05866c6a9f6c0bfc04accbdc8f0fde79fa756fb1
adreasnow <adrea.snow@gmail.com>
2026-04-23 12:29:41 +1000

    Update cluster.tf

--- tofu/cluster.tf
+++ tofu/cluster.tf
@@ -2,3 +2,16 @@
 #   enabled         = true
 #   expose_services = true
 # }
+
+# Only needed for orbstack
+resource "kubernetes_labels" "node" {
+  api_version = "v1"
+  kind        = "Node"
+  metadata {
+    name = "orbstack"
+  }
+  labels = {
+    "topology.kubernetes.io/region" = "us-east-1"
+    "topology.kubernetes.io/zone"   = "us-east-1a"
+  }
+}
